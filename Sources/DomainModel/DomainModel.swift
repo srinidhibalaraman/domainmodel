dc084898a50b9da27e94375cc455138c00023f8d
srinidhibalaraman <srinidhi.balaraman@gmail.com>
2022-04-22 21:06:05 -0700

    fixed domain model

--- Sources/DomainModel/DomainModel.swift
+++ Sources/DomainModel/DomainModel.swift
@@ -77,42 +77,43 @@ public class Job {
     
     var title : String
     var type : JobType
+
     
-    init(title: String, type: JobType) {
-        self.title = title
-        self.type = type
-    }
     public enum JobType {
         case Hourly(Double)
         case Salary(UInt)
     }
-    
-    
+
+    init(title: String, type: JobType) {
+        self.title = title
+        self.type = type
+    }
+
     public func calculateIncome(_ job : Int) -> UInt {
         switch self.type {
         case .Hourly(let val) :
-               return UInt(val) * 2000
-            
+               return UInt(val * Double(job))
+
         case .Salary(let val) :
-                return val
+                return UInt(val)
         }
     }
     
-    public func raise (byAmount: Double) {
+    public func raise(byAmount val: Double) {
         switch type {
-        case .Hourly(let val) :
-            self.type = JobType.Hourly(val + Double(byAmount))
-        case .Salary(let val) :
-            self.type = JobType.Salary(UInt(val + UInt(byAmount)))
+        case .Hourly(let res) :
+            self.type = JobType.Hourly(res + val)
+        case .Salary(let res) :
+            self.type = JobType.Salary(UInt(res + UInt(val)))
         }
     }
     
-    public func raise (byPercent: Double) {
+    public func raise(byPercent val: Double) {
         switch type {
-        case .Hourly(let val) :
-            self.type = JobType.Hourly(val + (100 * byPercent))
-        case .Salary(let val) :
-            self.type = JobType.Salary(UInt(val + UInt(100 * byPercent)))
+        case .Hourly(let res) :
+            self.type = JobType.Hourly(res + (res * val))
+        case .Salary(let res) :
+            self.type = JobType.Salary(UInt(res +  (res * UInt(val * 10))/10))
         }
     }
 }
@@ -124,23 +125,6 @@ public class Person {
     var firstName : String
     var lastName : String
     var age : Int
-    var job : Job? {
-        get {
-            return self.job
-        }
-        set(val) {
-            self.job = val
-        }
-    }
-    
-    var spouse : Person? {
-        get {
-            return self.spouse
-        }
-        set(val) {
-            self.spouse = val
-        }
-    }
     
     init(firstName: String, lastName: String, age: Int) {
         self.firstName = firstName
@@ -148,9 +132,38 @@ public class Person {
         self.age = age
     }
     
-    func toString() -> String {
-        return "[Person: firstName: \(self.firstName) lastName: \(self.lastName) age: \(self.age) job: \(String(describing: self.job)) spouse: \(String(describing: self.spouse))]"
+    var _job : Job? = nil
+    var job : Job? {
+        get {
+            return self._job;
+        }
+        set(value) {
+            if(self.age >= 16) {
+                self._job = value;
+            } else {
+                self._job = nil;
+            }
+        }
     }
+    
+    var _spouse : Person? = nil
+    var spouse : Person? {
+        get {
+            return self._spouse;
+        }
+        set(value) {
+            if(self.age >= 21) {
+                self._spouse = value;
+            } else {
+                self._spouse = nil;
+            }
+        }
+    }
+    
+
+    func toString() -> String {
+        return "[Person: firstName:\(self.firstName) lastName:\(self.lastName) age:\(self.age) job:\(String(describing: self._job)) spouse:\(String(describing: self._spouse))]"
+      }
     
     
     
@@ -161,7 +174,7 @@ public class Person {
 //
 public class Family {
     var members : [Person] = []
-    
+
     init(spouse1: Person, spouse2: Person) {
         if(spouse1.spouse == nil && spouse2.spouse == nil) {
             spouse1.spouse = spouse2
@@ -170,17 +183,17 @@ public class Family {
             self.members.append(spouse2)
         }
     }
-    
+
     func haveChild(_ child: Person) -> Bool {
         for member in self.members {
-            if(member.age >= 21 && member.spouse != nil) {
-                self.members.append(child)
-                return true
-            }
-        }
-        return false
+               if(member.age >= 21 && member.spouse != nil) {
+                   self.members.append(child);
+                   return true;
+               }
+           }
+        return false;
     }
-    
+
     func householdIncome() -> Int {
         var res = 0;
         for member in self.members {
